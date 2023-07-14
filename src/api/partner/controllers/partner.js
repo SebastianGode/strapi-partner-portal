a71dcb4fa728c4b5d57d231c7acb3371f3b82079
SebastianGode <sebastian@gode-familie.de>
2023-07-14 11:47:44 +0000

    Populate

--- src/api/partner/controllers/partner.js
+++ src/api/partner/controllers/partner.js
@@ -10,8 +10,9 @@ module.exports = createCoreController('api::partner.partner', ({strapi}) => ({
     async findOne(context) {
         const partner_id = context.params.id
 
-        const entity = await strapi.db.query('api::partner.partner').findOne({
-            where: { partner_id }
+        const entity = await strapi.db.query('api::partner.partner').findMany({
+            where: { partner_id },
+            populate: true
         })
 
         const sanitizedEntity = await this.sanitizeOutput(entity)
